6540c40f92820cab2355cf31ff668ecd1a1da73c
Oskar Svensson <oskar_erik@hotmail.com>
2017-04-27 11:25:49 +0200

    Added methods to Component class

--- Chef Engine/Chef Engine/Component.h
+++ Chef Engine/Chef Engine/Component.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Object.h"
+#include "LuaBridgeBinder.h"
 
 #include <SFML\Graphics.hpp>
 
@@ -9,6 +10,9 @@ namespace ce
 {
 	class Component : public ce::Object
 	{
+		// Befriends the templated Bind function so it can access our protected functions
+		friend void LuaBridgeBinder::Bind<ce::Component>(lua_State*);
+
 	public:
 		Component();
 		~Component();
@@ -19,15 +23,20 @@ namespace ce
 
 		// Getter for the 'hash'-variable
 		int GetHashCode() const;
+		void SetHashCode(int hash);
 
 		// Getter and setter for the 'enabled'-variable
 		void SetEnabled(bool enabled);
 		bool GetEnabled() const;
 
 		// Getter for 'gameObject'-variable
-		//GameObject* GetGameObject() const;
+		GameObject* GetGameObject() const;
 
 		bool operator==(const Component& other);
+
+	protected: 
+		// Binds all relevant members of this class with LuaBridge
+		static void DoBind(lua_State* L);
 
 	private: 
 
